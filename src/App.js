cbdb85e4b7178c158dab978dabd678a706ad2143
Keunil Song <keunilsong1021@gmail.com>
2024-01-02 02:56:12 +0900

    1월 2일,effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+  const onClick = () => setCounter((prev) => prev + 1);
+  const iRunOnlyOnce = () => { console.log("i run only once") };
+  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    if (counter < 5) {
+      console.log("counter is", counter);
+    }
+  }, [counter]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here.." />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
